1d051c82086a5e2b5b32ed1bb10d588c92b064e4
Jack Herrington <jherr@pobox.com>
2022-01-16 06:54:29 -0800

    Updates

---
+++ packages/product/src/ProductContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>Product Content</div>
+    <Hello />
+  </div>
+</template>
+
+<script>
+import Hello from "./Hello.vue";
+
+export default {
+  components: {
+    Hello
+  }
+};
+</script>
--- product/src/ProductContent.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>Product Content</div>
-</template>
